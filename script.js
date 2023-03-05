8bd9d8d3bb198bd0be24da43ab7fdee7af5097c6
Oleg Paramonov <68478325+oregu1@users.noreply.github.com>
2023-03-05 23:04:57 +0700

    initial commit

---
+++ script.js
@@ -0,0 +1,75 @@
+//REFERENCES
+const collapseSpan = document.querySelector(".collapse-icon");
+const employeePositions = document.querySelector(".employee-positions");
+const addedPositions = document.querySelector(".added-positions");
+const positionsTemplate = document.querySelector("[data-position-card]");
+const addedPositionsTemplate = document.querySelector("[data-added-position-card]");
+const searchInput = document.getElementById("search-input");
+
+//ARRAYS
+const positions = [];//сюда добавляются все должностя при обработке fetch запроса
+const addedPositionsArr = [];
+
+//SPAN справа на начальном экране
+collapseSpan.addEventListener('click', async () => {
+    
+   await fetch('../json_file.json')
+    .then(res => res.json())
+    .then(data => {
+        for(let i = 0; i< data.length; i++) {
+            positions.push(data[i])
+        }
+        renderPositions(positions)
+    });
+    
+    searchInput.value = "";
+})
+
+
+
+//INTERACTIONS
+searchInput.addEventListener('input', () => searchPositions(positions, searchInput.value));
+
+//FUNCTIONS
+const renderPositions = (positions) => {
+    for(let i = 0; i < positions.length; i++) {
+        const card = positionsTemplate.content.cloneNode(true).children[0];
+        // console.log(card)
+        card.children[0].textContent = positions[i].name;
+        card.children[1].addEventListener('click', addToAddedPositionsArray)
+
+        employeePositions.appendChild(card) ;
+    }
+}
+
+const searchPositions = (arrayOfPositions, searchText) => {
+
+    removeElements();//перерисовываем отобрааемые данные при каждом вводе в инпут
+
+    for(let i = 0; i < arrayOfPositions.length; i++) {
+        if(arrayOfPositions[i].name.toLowerCase().startsWith(searchText.toLowerCase()) && searchText != "") {
+            employeePositions.innerHTML = `
+                <div class="employee-position-card">
+                    <div class="employee-position">${arrayOfPositions[i].name}</div>
+                    <button class="btn">Добавить</button>
+                </div>
+            `;
+        }
+        
+    }
+}
+
+const removeElements = () => {
+    let items = document.querySelectorAll(".employee-position-card");
+    items.forEach(item => {
+     item.remove();
+    })
+ }
+
+const addToAddedPositionsArray = (e) => {
+    const par = e.target.parentElement.children[0];
+
+    const cardToAdd = addedPositionsTemplate.content.cloneNode(true).children[0];//div.position-card
+    cardToAdd.children[0].children[0].textContent = par.textContent;
+    addedPositions.appendChild(cardToAdd)
+}
