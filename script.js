c51d54a999fe20ea74061ba75e03a7591391ad57
Oleg Paramonov <68478325+oregu1@users.noreply.github.com>
2023-03-05 23:10:30 +0700

    added some info-comments

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
 //REFERENCES
 const collapseSpan = document.querySelector(".collapse-icon");
+
 const employeePositions = document.querySelector(".employee-positions");
 const addedPositions = document.querySelector(".added-positions");
+
 const positionsTemplate = document.querySelector("[data-position-card]");
 const addedPositionsTemplate = document.querySelector("[data-added-position-card]");
+
 const searchInput = document.getElementById("search-input");
 
 //ARRAYS
@@ -25,8 +28,6 @@ collapseSpan.addEventListener('click', async () => {
     searchInput.value = "";
 })
 
-
-
 //INTERACTIONS
 searchInput.addEventListener('input', () => searchPositions(positions, searchInput.value));
 
@@ -34,7 +35,6 @@ searchInput.addEventListener('input', () => searchPositions(positions, searchInp
 const renderPositions = (positions) => {
     for(let i = 0; i < positions.length; i++) {
         const card = positionsTemplate.content.cloneNode(true).children[0];
-        // console.log(card)
         card.children[0].textContent = positions[i].name;
         card.children[1].addEventListener('click', addToAddedPositionsArray)
 
@@ -46,6 +46,7 @@ const searchPositions = (arrayOfPositions, searchText) => {
 
     removeElements();//перерисовываем отобрааемые данные при каждом вводе в инпут
 
+    //отрисовываем те элементы, которые соответствуют вводу
     for(let i = 0; i < arrayOfPositions.length; i++) {
         if(arrayOfPositions[i].name.toLowerCase().startsWith(searchText.toLowerCase()) && searchText != "") {
             employeePositions.innerHTML = `
@@ -59,6 +60,7 @@ const searchPositions = (arrayOfPositions, searchText) => {
     }
 }
 
+//удаление элементов во время ввода в инпут
 const removeElements = () => {
     let items = document.querySelectorAll(".employee-position-card");
     items.forEach(item => {
@@ -66,10 +68,11 @@ const removeElements = () => {
     })
  }
 
+ //добавляем должность в список добавленных позиций
 const addToAddedPositionsArray = (e) => {
     const par = e.target.parentElement.children[0];
 
-    const cardToAdd = addedPositionsTemplate.content.cloneNode(true).children[0];//div.position-card
+    const cardToAdd = addedPositionsTemplate.content.cloneNode(true).children[0];//div.position-card из template
     cardToAdd.children[0].children[0].textContent = par.textContent;
     addedPositions.appendChild(cardToAdd)
 }
